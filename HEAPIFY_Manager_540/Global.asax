3d2a5e3e0b209748f3544f93e207808fa4c1a758
Ammara Khalid <Ammara Khalid>
2017-11-11 23:06:27 -0500

    Add project files.

---
+++ HEAPIFY_Manager_540/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HEAPIFY_Manager_540.MvcApplication" Language="C#" %>
---
+++ HEAPIFY_Manager_540/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HEAPIFY_Manager_540.Startup))]
+namespace HEAPIFY_Manager_540
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
