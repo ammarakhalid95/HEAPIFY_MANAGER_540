0c4395c38981c3cd37ab5581f9ec3d431a592b41
Ammara Khalid <ammarak95@hotmail.com>
2017-11-16 15:12:28 -0500

    Test to controllers and patients

---
+++ HEAPIFY_Manager_540/Controllers/MedicalAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HEAPIFY_Manager_540.Controllers
+{
+    public class MedicalAdminController : Controller
+    {
+        // GET: MedicalAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
